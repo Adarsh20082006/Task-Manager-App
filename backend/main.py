96de1580e41bfa413f6b9eaa0d901308eb969356
Adarsh M M <adarshakashmm@gmail.com>
2026-03-28 10:20:11 +0530

    fix: installed cryptogrphy package to secure the password by pymysql.

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,10 @@
 from fastapi import FastAPI
+from routes import task_routes
 from db.db_config import engine, SessionLocal, Base
 from sqlalchemy import text
 from db.models import Task
 app = FastAPI()
 
-#TEST DB CONNECTION
 Base.metadata.create_all(bind=engine)
 
-#log db connection
-
-@app.get("/")   
-def index():
-    try:
-        db = SessionLocal()
-        query = text("SELECT 1")
-        db.execute(query)
-        print("Database connection successful!")
-    except Exception as e:
-        print(f"Database connection failed: {e}")
-    return {"message": "Hello, World!"}
+app.include_router(task_routes.router)
